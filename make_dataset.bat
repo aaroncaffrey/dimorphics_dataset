2a4e2657bec232663c0eecc5fe13c6be8fc5bac1
aaroncaffrey <aaroncaffrey@gmail.com>
2020-02-07 10:25:58 +0000

    updates

--- make_dataset.bat
+++ make_dataset.bat
@@ -1,55 +1,29 @@
-md e:\dataset\2i\dimorphic_coil\
-md e:\dataset\2n\dimorphic_coil\
-md e:\dataset\2p\dimorphic_coil\
-md e:\dataset\3i\dimorphic_coil\
-md e:\dataset\3n\dimorphic_coil\
-md e:\dataset\3p\dimorphic_coil\
-md e:\dataset\2i\standard_coil\
-md e:\dataset\2n\standard_coil\
-md e:\dataset\2p\standard_coil\
-md e:\dataset\3i\standard_coil\
-md e:\dataset\3n\standard_coil\
-md e:\dataset\3p\standard_coil\
+set exe=C:\Users\Aaron\Desktop\dimorphics_dataset\dimorphics_dataset\bin\x64\Release\netcoreapp3.1\dimorphics_dataset.exe
+set output=e:\dataset
+md %output%\
 
+:: 2d interface subsequence area:
+:: %exe% -area=2i -use_dssp3=true -class_id=+1 -class_name=dimorphic_coil -min_sequence_length=3 -max_features=100 -output_folder=%output%\ -verbose=true 1> %output%\stdout_2i_(+1)_(dimorphic_coil).txt 2> %output%\stderr_2i_(+1)_(dimorphic_coil).txt
+:: %exe% -area=2i -use_dssp3=true -class_id=-1 -class_name=standard_coil -min_sequence_length=3 -max_features=100 -output_folder=%output%\ -verbose=true 1> %output%\stdout_2i_(-1)_(standard_coil).txt 2> %output%\stderr_2i_(-1)_(standard_coil).txt
 
-:: 2d
+:: 2d neighbourhood area:
+:: %exe% -area=2n -use_dssp3=true -class_id=+1 -class_name=dimorphic_coil -min_sequence_length=3 -max_features=100 -output_folder=%output%\ -verbose=true 1> %output%\stdout_2n_(+1)_(dimorphic_coil).txt 2> %output%\stderr_2n_(+1)_(dimorphic_coil).txt
+%exe% -area=2n -use_dssp3=true -class_id=-1 -class_name=standard_coil -min_sequence_length=3 -max_features=100 -output_folder=%output%\ -verbose=true 1> %output%\stdout_2n_(-1)_(standard_coil).txt 2> %output%\stderr_2n_(-1)_(standard_coil).txt
 
-:: DC 2d interface subsequence area:
-"C:\Users\Aaron\Desktop\dimorphics_dataset\dimorphics_dataset\bin\x64\Release\netcoreapp3.1\dimorphics_dataset.exe" -area=2i -use_dssp3=true -class_id=+1 -class_name=dimorphic_coil -min_sequence_length=3 -max_features=100 -output_folder=e:\dataset\2i\dimorphic_coil\ 1> e:\dataset\2i\dimorphic_coil\stdout.txt 2> e:\dataset\2i\dimorphic_coil\stderr.txt
-:: SC 2d interface subsequence area:
-"C:\Users\Aaron\Desktop\dimorphics_dataset\dimorphics_dataset\bin\x64\Release\netcoreapp3.1\dimorphics_dataset.exe" -area=2i -use_dssp3=true -class_id=-1 -class_name=standard_coil -min_sequence_length=3 -max_features=100 -output_folder=e:\dataset\2i\standard_coil\ 1> e:\dataset\2i\standard_coil\stdout.txt 2> e:\dataset\2i\standard_coil\stderr.txt
+:: 2d protein chain area:
+%exe% -area=2p -use_dssp3=true -class_id=+1 -class_name=dimorphic_coil -min_sequence_length=3 -max_features=100 -output_folder=%output%\ -verbose=true 1> %output%\stdout_2p_(+1)_(dimorphic_coil).txt 2> %output%\stderr_2p_(+1)_(dimorphic_coil).txt
+%exe% -area=2p -use_dssp3=true -class_id=-1 -class_name=standard_coil -min_sequence_length=3 -max_features=100 -output_folder=%output%\ -verbose=true 1> %output%\stdout_2p_(-1)_(standard_coil).txt 2> %output%\stderr_2p_(-1)_(standard_coil).txt
 
+:: 3d interface subsequence area:
+%exe% -area=3i -use_dssp3=true -class_id=+1 -class_name=dimorphic_coil -min_sequence_length=3 -max_features=100 -output_folder=%output%\ -verbose=true 1> %output%\stdout_3i_(+1)_(dimorphic_coil).txt 2> %output%\stderr_3i_(+1)_(dimorphic_coil).txt
+%exe% -area=3i -use_dssp3=true -class_id=-1 -class_name=standard_coil -min_sequence_length=3 -max_features=100 -output_folder=%output%\ -verbose=true 1> %output%\stdout_3i_(-1)_(standard_coil).txt 2> %output%\stderr_3i_(-1)_(standard_coil).txt
 
-:: DC 2d neighbourhood area:
-"C:\Users\Aaron\Desktop\dimorphics_dataset\dimorphics_dataset\bin\x64\Release\netcoreapp3.1\dimorphics_dataset.exe" -area=2n -use_dssp3=true -class_id=+1 -class_name=dimorphic_coil -min_sequence_length=3 -max_features=100 -output_folder=e:\dataset\2n\dimorphic_coil\ 1> e:\dataset\2n\dimorphic_coil\stdout.txt 2> e:\dataset\2n\dimorphic_coil\stderr.txt
-:: SC 2d neighbourhood area:
-"C:\Users\Aaron\Desktop\dimorphics_dataset\dimorphics_dataset\bin\x64\Release\netcoreapp3.1\dimorphics_dataset.exe" -area=2n -use_dssp3=true -class_id=-1 -class_name=standard_coil -min_sequence_length=3 -max_features=100 -output_folder=e:\dataset\2n\standard_coil\ 1> e:\dataset\2n\standard_coil\stdout.txt 2> e:\dataset\2n\standard_coil\stderr.txt
+:: 3d neighbourhood area:
+%exe% -area=3n -use_dssp3=true -class_id=+1 -class_name=dimorphic_coil -min_sequence_length=3 -max_features=100 -output_folder=%output%\ -verbose=true 1> %output%\stdout_3n_(+1)_(dimorphic_coil).txt 2> %output%\stderr_3n_(+1)_(dimorphic_coil).txt
+%exe% -area=3n -use_dssp3=true -class_id=-1 -class_name=standard_coil -min_sequence_length=3 -max_features=100 -output_folder=%output%\ -verbose=true 1> %output%\stdout_3n_(-1)_(standard_coil).txt 2> %output%\stderr_3n_(-1)_(standard_coil).txt
 
-
-:: DC 2d protein area:
-"C:\Users\Aaron\Desktop\dimorphics_dataset\dimorphics_dataset\bin\x64\Release\netcoreapp3.1\dimorphics_dataset.exe" -area=2p -use_dssp3=true -class_id=+1 -class_name=dimorphic_coil -min_sequence_length=3 -max_features=100 -output_folder=e:\dataset\2p\dimorphic_coil\ 1> e:\dataset\2p\dimorphic_coil\stdout.txt 2> e:\dataset\2p\dimorphic_coil\stderr.txt
-:: SC 2d protein area:
-"C:\Users\Aaron\Desktop\dimorphics_dataset\dimorphics_dataset\bin\x64\Release\netcoreapp3.1\dimorphics_dataset.exe" -area=2p -use_dssp3=true -class_id=-1 -class_name=standard_coil -min_sequence_length=3 -max_features=100 -output_folder=e:\dataset\2p\standard_coil\ 1> e:\dataset\2p\standard_coil\stdout.txt 2> e:\dataset\2p\standard_coil\stderr.txt
-
-
-:: 3d
-
-:: DC 3d interface subsequence area:
-"C:\Users\Aaron\Desktop\dimorphics_dataset\dimorphics_dataset\bin\x64\Release\netcoreapp3.1\dimorphics_dataset.exe" -area=3i -use_dssp3=true -class_id=+1 -class_name=dimorphic_coil -min_sequence_length=3 -max_features=100 -output_folder=e:\dataset\3i\dimorphic_coil\ 1> e:\dataset\3i\dimorphic_coil\stdout.txt 2> e:\dataset\3i\dimorphic_coil\stderr.txt
-:: SC 3d interface subsequence area:
-"C:\Users\Aaron\Desktop\dimorphics_dataset\dimorphics_dataset\bin\x64\Release\netcoreapp3.1\dimorphics_dataset.exe" -area=3i -use_dssp3=true -class_id=-1 -class_name=standard_coil -min_sequence_length=3 -max_features=100 -output_folder=e:\dataset\3i\standard_coil\ 1> e:\dataset\3i\standard_coil\stdout.txt 2> e:\dataset\3i\standard_coil\stderr.txt
-
-
-:: DC 3d neighbourhood area:
-"C:\Users\Aaron\Desktop\dimorphics_dataset\dimorphics_dataset\bin\x64\Release\netcoreapp3.1\dimorphics_dataset.exe" -area=3n -use_dssp3=true -class_id=+1 -class_name=dimorphic_coil -min_sequence_length=3 -max_features=100 -output_folder=e:\dataset\3n\dimorphic_coil\ 1> e:\dataset\3n\dimorphic_coil\stdout.txt 2> e:\dataset\3n\dimorphic_coil\stderr.txt
-:: SC 3d neighbourhood area:
-"C:\Users\Aaron\Desktop\dimorphics_dataset\dimorphics_dataset\bin\x64\Release\netcoreapp3.1\dimorphics_dataset.exe" -area=3n -use_dssp3=true -class_id=-1 -class_name=standard_coil -min_sequence_length=3 -max_features=100 -output_folder=e:\dataset\3n\standard_coil\ 1> e:\dataset\3n\standard_coil\stdout.txt 2> e:\dataset\3n\standard_coil\stderr.txt
-
-
-:: DC 3d protein area:
-"C:\Users\Aaron\Desktop\dimorphics_dataset\dimorphics_dataset\bin\x64\Release\netcoreapp3.1\dimorphics_dataset.exe" -area=3p -use_dssp3=true -class_id=+1 -class_name=dimorphic_coil -min_sequence_length=3 -max_features=100 -output_folder=e:\dataset\3p\dimorphic_coil\ 1> e:\dataset\3p\dimorphic_coil\stdout.txt 2> e:\dataset\3p\dimorphic_coil\stderr.txt
-:: SC 3d protein area:
-"C:\Users\Aaron\Desktop\dimorphics_dataset\dimorphics_dataset\bin\x64\Release\netcoreapp3.1\dimorphics_dataset.exe" -area=3p -use_dssp3=true -class_id=-1 -class_name=standard_coil -min_sequence_length=3 -max_features=100 -output_folder=e:\dataset\3p\standard_coil\ 1> e:\dataset\3p\standard_coil\stdout.txt 2> e:\dataset\3p\standard_coil\stderr.txt
-
+:: 3d protein chain area:
+%exe% -area=3p -use_dssp3=true -class_id=+1 -class_name=dimorphic_coil -min_sequence_length=3 -max_features=100 -output_folder=%output%\ -verbose=true 1> %output%\stdout_3p_(+1)_(dimorphic_coil).txt 2> %output%\stderr_3p_(+1)_(dimorphic_coil).txt
+%exe% -area=3p -use_dssp3=true -class_id=-1 -class_name=standard_coil -min_sequence_length=3 -max_features=100 -output_folder=%output%\ -verbose=true 1> %output%\stdout_3p_(-1)_(standard_coil).txt 2> %output%\stderr_3p_(-1)_(standard_coil).txt
 
 pause
--- run/generate_datasets_1x_2n.bat
+++
@@ -1,3 +0,0 @@
-..\dimorphics_dataset\bin\x64\Release\netcoreapp3.1\dimorphics_dataset.exe 2n
-
-pause
